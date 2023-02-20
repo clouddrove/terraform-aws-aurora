3407df34f1c393091334bbb2a055e5ab1847870b
anmolnagpal <anmol@clouddrove.com>
2023-02-20 21:16:23 +0530

    use terraform letast version

--- _example/aurora-mysql/example.tf
+++ _example/aurora-mysql/example.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "clouddrove/vpc/aws"
-  version = "0.15.1"
+  version = "1.3.0"
 
   name        = "aurora-mysql"
   environment = "test"
@@ -14,7 +14,7 @@ module "vpc" {
 
 module "public_subnets" {
   source      = "clouddrove/subnet/aws"
-  version     = "0.15.3"
+  version     = "1.3.0"
   name        = "public-subnet"
   environment = "test"
   label_order = ["name", "environment"]
@@ -30,7 +30,7 @@ module "public_subnets" {
 
 module "security-group" {
   source  = "clouddrove/security-group/aws"
-  version = "1.0.1"
+  version = "1.3.0"
   name    = "aurora-sg"
 
   environment   = "test"
