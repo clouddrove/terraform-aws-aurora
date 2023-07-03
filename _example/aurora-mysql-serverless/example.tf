0f280625e1d24aad84d46009ad1174337e5d5473
mamraj yadav <themamraj0131@gmail.com>
2023-07-03 22:37:11 +0530

    bug: updated and added github action file

--- _example/aurora-mysql-serverless/example.tf
+++ _example/aurora-mysql-serverless/example.tf
@@ -26,12 +26,12 @@ module "subnets" {
   source  = "clouddrove/subnet/aws"
   version = "1.3.0"
 
-  name        = "public-subnet"
+  name        = "subnets"
   environment = "test"
   label_order = ["name", "environment"]
 
 
-  availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  availability_zones  = ["eu-west-1a", "eu-west-1b"]
   vpc_id              = module.vpc.vpc_id
   cidr_block          = module.vpc.vpc_cidr_block
   ipv6_cidr_block     = module.vpc.ipv6_cidr_block
